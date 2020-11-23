dcf5e7256d18949143c03cbd146898f335019d10
Aseef Ahmed <aseefahmed@gmail.com>
2020-11-24 08:42:12 +1300

    Update user_data.sh

--- terraform/modules/user_data.sh
+++ terraform/modules/user_data.sh
@@ -2,6 +2,6 @@
 sudo yum update -y
 sudo yum install git -y
 git clone https://github.com/aseefahmed/mywordpress.git
-sudo rm -f /var/www/html/*
-sudo cp -r mywordpress/* /var/www/html/
-
+cd mywordpress
+docker-compose build
+docker-compose up -d
